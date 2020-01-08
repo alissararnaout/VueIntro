dd7e178d5fe74a202834de23a8deda78043e7405
Alissar Arnaout <Alissar@Alissars-Air.fc.ca>
2020-01-08 10:36:37 -0500

    vue setup

---
+++ js/main.js
@@ -0,0 +1,24 @@
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
+
+// *IMPORTANT* set up a viewmodel and use the Vue framework 
+const my_vm = (() => {
+    //variables or define components, etc.
+
+    let vue_VM = new Vue({
+        data: {
+            message: "Hello from Vue!",
+
+            collection: [ // array of objects
+                {name: "Joe", role: "Prof"},
+                {name: "John", role: "Prof"},
+                {name: "Jarrod", role: "Prof"},
+            ]
+        },
+
+        methods: {
+            logClicked() {
+                console.log('clicked on an element');
+            }
+        }
+    }).$mount("#app"); // telling it to find element in index (so in this case div #app)
+})();
